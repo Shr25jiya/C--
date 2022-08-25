8093f71a45595d766ae98332423a15726116eebd
shruti <be20cs102shruti@gmail.com>
2022-08-25 20:00:23 +0530

    variable types

--- fourth.cpp
+++ fourth.cpp
@@ -9,6 +9,7 @@ int b=55;*/
 int a=22,b=55;
 cout<<"variable a stores "<<a<<".\n variable b stores "<<b<<".\n variable scopes";
 float f=0.12;
-cout<<".\n variable f stores float type "<<f;
+char c='a';
+cout<<".\n variable c stores "<<c<<".\n variable f contains "<<f;
 return 0;
 }
