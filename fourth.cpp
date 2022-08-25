b3196fccb51c77738537cfc6a9bfdbe4775bb654
shruti <be20cs102shruti@gmail.com>
2022-08-25 19:54:50 +0530

    it holds value

---
+++ fourth.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+/*int a=22;
+int b=55;*/
+int a=22,b=55;
+cout<<"variable a stores "<<a<<".\n variable b stores "<<b<<".\n variable scopes";
+float f=0.12;
+cout<<".\n variable f stores float type "<<f;
+return 0;
+}
