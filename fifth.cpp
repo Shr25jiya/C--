261b1e9e0af08cdd86f9bc7288d858853f206c32
shruti <be20cs102shruti@gmail.com>
2022-08-25 20:18:14 +0530

    global and local variable

---
+++ fifth.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int glo=56;
+void sum()
+{
+int a=1;
+cout<<glo;
+}
+
+int main()
+{
+    bool bl=true;
+  int glo=7;
+  glo=98;
+  cout<<glo<<bl;
+  return 0;
+ 
+}
